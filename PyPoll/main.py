43a7f2ada41fbca056f4299882c43a543262764b
ahab91 <130241483+ahab91@users.noreply.github.com>
2023-05-04 21:11:52 -0400

    Fourth Commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -26,61 +26,51 @@ with open(election_csv) as csvfile:
         # Add candidate
         cand.append(row[2])
         
+        # Extract candidate name
+        candidate = row[2]
+        
+winning_count = 0
+winner = ""
+        
 # Variable for total count of votes
 total_count = len(id)
 
 # List for unique candidate names
 cand_names = []
-for name in cand:
-    if name not in cand_names:
-        cand_names.append(name)
+cand_votes = {}
+for candidate in cand:
+    if candidate not in cand_names:
+        cand_names.append(candidate)
+        cand_votes[candidate] = 0
+    cand_votes[candidate] = cand_votes[candidate] + 1
 
-stockham_vote = 0
-degette_vote = 0
-doane_vote = 0
-winner_name = []
 
-for vote in cand:
-    if vote == cand_names[0]:
-        stockham_vote = stockham_vote + 1
-    if vote == cand_names[1]:
-        degette_vote = degette_vote + 1
-    if vote == cand_names[2]:
-        doane_vote = doane_vote + 1
-
-for winner in cand_names:
-    if stockham_vote > degette_vote and doane_vote:
-        winner = cand_names[0]
-    if degette_vote > stockham_vote and doane_vote:
-        winner = cand_names[1]
-    if doane_vote > stockham_vote and degette_vote:
-        winner = cand_names[2]
-    
-
-ccs_percent = round((stockham_vote) / (total_count) * 100, 3)
-dd_percent = round((degette_vote) / (total_count) * 100, 3)
-rad_percent = round((doane_vote) / (total_count) * 100, 3)
-
-win_count = max(stockham_vote, degette_vote, doane_vote)
-winner_percent = max(ccs_percent, dd_percent, rad_percent)
-
-output_file = os.path.join("Analysis", "election_results.txt")
-
-lines = ["Election Results", "", "------------------------", "", "Total Votes: " + str(total_count), "------------------------", str(cand_names[0]) + ": " + str(ccs_percent)+'%' + " (" + str(stockham_vote) + ")", str(cand_names[1]) + ": " + str(dd_percent)+'%' + " (" + str(degette_vote) + ")", str(cand_names[2]) + ": " + str(rad_percent)+'%' + " (" + str(doane_vote) + ")", "", "------------------------", "", "Winner: " + str(winner), "", "------------------------"]
-
-with open(output_file, "w") as f:
-    for line in lines:
-        f.write(line)
-        f.write('\n')
-
-# Print to terminal
 print("Election Results")
 print("------------------------")
 print("Total Votes: " + str(total_count))
 print("------------------------")
-print(str(cand_names[0]) + ": " + str(ccs_percent)+'%' + " (" + str(stockham_vote) + ")")
-print(str(cand_names[1]) + ": " + str(dd_percent)+'%' + " (" + str(degette_vote) + ")")
-print(str(cand_names[2]) + ": " + str(rad_percent)+'%' + " (" + str(doane_vote) + ")")
+output_file = os.path.join("Analysis", "election_results.txt")
+
+with open(output_file, 'w') as f:
+    f.write("Election Results"'\n')
+    f.write("------------------------"'\n')
+    f.write("Total Votes: " + str(total_count))
+    f.write('\n'"------------------------"'\n')
+    for cand_opt in cand_votes:
+        votes = cand_votes.get(cand_opt)
+        pct = round((((votes) / total_count) * 100), 3)
+        if (votes > winning_count):
+            winning_count = votes
+            winner = cand_opt
+        print(str(cand_opt) + ": ", str(pct) + "% ", "(" + str(votes) + ")")
+        f.write('\n'"" + str(cand_opt) + ": ")
+        f.write(str(pct) + "% ")
+        f.write("(" + str(votes) + ")"'\n')
+    f.write('\n'"------------------------"'\n')
+    f.write('\n'"Winner: " + winner)
+
 print("------------------------")
 print("Winner: " + str(winner))
 print("------------------------")
+    
+
